b523c37cd7f1579da4445f336092414cb6cef69c
Evan Sia <ws-91@hotmail.com>
2020-05-11 22:58:30 +0100

    Fixup

--- src/app/interceptor.ts
+++ src/app/interceptor.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, switchMap } from 'rxjs/operators';
 import { ClownService } from './clown.service';
@@ -10,9 +10,12 @@ export class Interceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(this.addToken(request)).pipe(catchError(error => {
-        if (this.clownService.isLoggedIn() && error.status == 401) {
+        if (error.status == 401) {
             return this.clownService.extendUserSession().pipe(
                 switchMap(() => {
+                    if (!this.clownService.isLoggedIn()) {
+                        return throwError(this.generateErrorResponse());
+                    }
                     return next.handle(this.addToken(request));
                 })
             );
@@ -20,6 +23,10 @@ export class Interceptor implements HttpInterceptor {
             return throwError(error);
         }
     }));
+  }
+
+  private generateErrorResponse() {
+      return new HttpErrorResponse({status: 401});
   }
 
   private addToken(request: HttpRequest<any>) {
